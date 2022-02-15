bbddd4a16457fc72721007e6a27744c5b05e3955
mozoroff <mozoshima@gmail.com>
2022-02-15 13:17:11 +0900

    init

---
+++ src/main/java/com/mozoroff/WhatDidYouEatApplication.java
@@ -0,0 +1,13 @@
+package com.mozoroff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatDidYouEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatDidYouEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozoroff/WhatDidYouEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozoroff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatDidYouEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
